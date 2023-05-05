bfdb64efe63809815ef4c12aed65d8d6ab65ef3d
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2023-05-05 23:05:02 +0200

    (add):Remote DB

--- db-connection/connection.js
+++ db-connection/connection.js
@@ -3,22 +3,37 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ahmadlotfygamersfield:"+process.env.DATABASE_PASSWORD+"cluster0.vljywdn.mongodb.net/?retryWrites=true&w=majority";
 
 const dbConnect = () => {
-  // Connect to the database
-  // const dbConnectionString = process.env.DATABASE.replace(
-  //   "<PASSWORD>",
-  //   process.env.DATABASE_PASSWORD
-  // );
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true, 
+    deprecationErrors: true,
+  }
+});
 
-  // REMOTE DATABASE
-  mongoose
-    .connect("mongodb+srv://ahmadlotfygamersfield:Bashera2@cluster0.vljywdn.mongodb.net/?retryWrites=true&w=majority", {
-      useNewUrlParser: true,
-    })
-    .then(() => {
-      console.log("Successfully connected to database");
-    });
-};
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+}
+
+
 
 module.exports = dbConnect;
--- server.js
+++ server.js
@@ -4,40 +4,12 @@ dotenv.config({ path: "./.env" });
 // Subscribe to unhandled rejection of any promise
 
 const app = require("./app");
-//dbConnect();
+dbConnect();
 
 
 
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ahmadlotfygamersfield:Bashera2@cluster0.vljywdn.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 // var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb+srv://ahmadlotfygamersfield:Bashera2@cluster0.vljywdn.mongodb.net/?retryWrites=true&w=majority";
